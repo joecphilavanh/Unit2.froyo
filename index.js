61107b2a5bde30587cb02fd7ea844054cea34664
Joe Philavanh <joecphilavanh@DESKTOP-PL0LMS4.localdomain>
2023-10-31 13:50:43 -0700

    final edit

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 // The user is prompted for froyo flavors upon entering the website.
 const input = prompt("Welcome to Joe's frozen palace. Please Enter a list of comma-separated froyo flavors:");
-
 // The user's input string is split into an array of strings.
 const flavors = input.split(",");
-
 // An object is used to keep count of how many orders there are of each flavor.
 const counter = {};
-
 // A loop is used to iterate through the array of flavors.
 for (let i = 0; i < flavors.length; i++) {
     const flavor = flavors[i].trim();
